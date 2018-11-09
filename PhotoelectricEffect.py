a04deaa7e28321b07f0541838cd82588429c407e
phil-shenk <shenk.philip@gmail.com>
2018-11-08 20:09:20 -0500

    updated (all proxy methods)
    fixed some graphs and showed more graphs of all 4 proxy methods

--- PhotoelectricEffect.py
+++ PhotoelectricEffect.py
@@ -83,13 +83,11 @@ def curveFitProxy(Varr, Iarr, maxSlopeIndex, flatFit, i):
     print('Fitted V0 value for '+str(i)+' is '+str(V0))
     diodeFit = I0*(np.exp(np.array(Varr)/V0) - 1)+b+(m*np.array(Varr))
     
-    labels=['i 0%','20%','40%','60%','80%','100%','f 0%']
     plt.figure(100+fileIndex)
     plt.ylim(-1,5)
-    plt.plot(Varr[0:len(Varr)-0], diodeFit, label=labels[i])
-    plt.legend(loc='best')
+    plt.plot(Varr[0:len(Varr)-0], diodeFit)
     plt.scatter(Varr, Iarr)
-
+    
     return diodeFit, V0 
     
 def xinterceptProxy(Varr, smoothI):
@@ -157,9 +155,6 @@ colorFrequencies = {
 'violet.dat':404.7,
 'ultraviolet.dat':365.4
 }
-markers = ['1','<','>','^','v','D','2']
-percentages = [0,20,40,60,80,100,0]
-
 for filename in os.listdir('data'):
     rows = []
     print('data/'+filename)
@@ -201,35 +196,23 @@ for filename in os.listdir('data'):
     
     
     #plot it
+    plt.title("Data from " +filename)
+    plt.ylabel("Y")
+    plt.xlabel("X")
     
-    thisColor_stoppingPotential = []
-    thisColor_stoppingPotential_i = []
-    thisColor_stoppingPotential_d = []
-    thisColor_stoppingPotential_x = []
-    theseFrequencies = []
     
+    percentages = [0,20,40,60,80,100,0]
     for i in range(len(voltageArrays)):
-        plt.figure(200+fileIndex)
-        plt.xlabel('V')
-        plt.ylabel('I')
-        plt.ylim(-1,5)
-        plt.title('V vs I raw data, '+filename)
-        plt.scatter(voltageArrays[i], currentArrays[i],marker=markers[i], label=str(percentages[i])+'%')
-        plt.legend(loc='upper left')
-        
         plt.figure(fileIndex)
         plt.xlabel('V')
         plt.ylabel('I')
         plt.ylim(-1,5)
-        #plt.title('V vs I with fits, '+filename)
-        plt.title("Line fit methods from " +filename)
-        plt.ylabel("Vr")
-        plt.xlabel("Ip")
+        plt.title('V vs I with fits, '+filename)
         #plt.plot(voltageArrays[i], currentArrays[i], label=(str(i)+': '+str(percentages)+'%'))
         flatFit, smoothI, smoothdIdV, slopeFit, diodeFit, exceedIndex, intersectionIndex, diodeFitV0, xintV0 = AnalyzeCurve(voltageArrays[i], currentArrays[i], i, percentages[i], fileIndex)
         plt.subplot(211)        
         plt.plot(voltageArrays[i], flatFit)
-        plt.plot(voltageArrays[i], smoothI)
+        plt.plot(voltageArrays[i], smoothI, label=str(percentages[i])+'%')
         plt.subplot(212)
         plt.plot(voltageArrays[i], flatFit)
         plt.plot(voltageArrays[i], smoothI, label=str(percentages[i])+'%')
@@ -238,103 +221,42 @@ for filename in os.listdir('data'):
         plt.subplot(212)
         plt.legend(loc='best')
         plt.scatter(voltageArrays[i][exceedIndex], smoothI[exceedIndex])
-        
         if not (percentages[i] == 0):
-            
             stoppingPotential = voltageArrays[i][exceedIndex]
-            thisColor_stoppingPotential.append(stoppingPotential)
-            theseFrequencies.append(colorFrequencies.get(filename))
+            stoppingPotentials_threshold.append(stoppingPotential)
+            frequencies.append(colorFrequencies.get(filename))
         
         plt.subplot(211)
        
-       
         #log stopping potentials from intersection method
-        
         plt.scatter(voltageArrays[i][intersectionIndex], flatFit[intersectionIndex])
         if not (percentages[i] == 0):
             stoppingPotential = voltageArrays[i][intersectionIndex]
-            thisColor_stoppingPotential_i.append(stoppingPotential)
+            stoppingPotentials_intercept.append(stoppingPotential)
             
             #also add diode fit V0
-            thisColor_stoppingPotential_d.append(diodeFitV0)
+            stoppingPotentials_diodefit.append(diodeFitV0)
             #also xintercept
-            thisColor_stoppingPotential_x.append(xintV0)
-        
-        print(stoppingPotentials_intercept)
-        print('^ real deal')
+            stoppingPotentials_xintFit.append(xintV0)
         
         plt.subplot(211)
-        plt.plot(voltageArrays[i], slopeFit, label=str(percentages[i])+'%')
+        plt.plot(voltageArrays[i], slopeFit)
         #plt.figure(1)
         #plt.xlabel('V')
         #plt.ylabel('smooth dI/dV')
         #plt.title('Derivative of Smoothed Current')
         #plt.plot(voltageArrays[i][0:len(voltageArrays[i])-1], smoothdIdV)
         plt.legend(loc='best')
-    stoppingPotentials_threshold.append(thisColor_stoppingPotential)
-    stoppingPotentials_intercept.append(thisColor_stoppingPotential_i)
-    stoppingPotentials_diodefit.append(thisColor_stoppingPotential_d)
-    stoppingPotentials_xintFit.append(thisColor_stoppingPotential_x)
-    frequencies.append(theseFrequencies)
+   
+
     fileIndex = fileIndex+1
     
 plt.figure(25)
 plt.xlabel('wavelength')
 plt.ylabel('stopping potential (V)')
-
-for i in range(len(frequencies)):
-    plt.figure(25)
-    plt.xlabel('wavelength')
-    plt.ylabel('stopping potential (V)')
-    for j in range(len(frequencies[i])):
-        plt.scatter(frequencies[i][j], stoppingPotentials_threshold[i][j], c='b', marker=markers[j], s=100)
-    plt.title('Stopping Potential vs Wavelength (Threshold method)')
-    
-    plt.figure(26)
-    plt.xlabel('wavelength')
-    plt.ylabel('stopping potential (V)')
-    plt.scatter(frequencies[i], stoppingPotentials_intercept[i], c='r', label='Intercept method')
-    plt.scatter(frequencies[i], stoppingPotentials_diodefit[i], c='y', label='Diode function (zero) method')
-    plt.scatter(frequencies[i], stoppingPotentials_xintFit[i], c='g', label='Intercept method')
-
-plt.figure(43)
-plt.xlabel("Intensity (%)")
-plt.ylabel("Stopping Potential (V)")
-plt.title("Stopping potential vs. Transmission")
-print(frequencies, 'that is frequencies')
-for i in range(len(frequencies)):
-    print(len(stoppingPotentials_threshold[i]))
-    print(len(percentages[1:6]))
-    plt.scatter(percentages[1:6], stoppingPotentials_threshold[i], marker=markers[i], label=str(frequencies[i][i]))
-plt.legend(loc='best')
-
-allPotentials = np.transpose(np.array(stoppingPotentials_threshold))
-#they are actually wavelengths in nm at this point
-allFrequencies = np.transpose(np.array(frequencies))
-allFrequencies = 299792458.0/(allFrequencies*(0.000000001)) #f = c/lambda
-
-plt.figure(300+i)
-plt.title('Stopping potential vs Frequency (with fits)')
-plt.xlabel('frequency')
-plt.ylabel('Stopping potential')
-freq_x = np.linspace(500000000000000.0, 850000000000000.0, 100)
-slopes = []
-yints = []
-for i in range(len(allPotentials)):
-    fit_params, fit_uncertainties = curve_fit(line, allFrequencies[i], allPotentials[i])
-    fit_Vs = line(freq_x, fit_params[0], fit_params[1])
-    slopes.append(fit_params[0])
-    yints.append(fit_params[1])
-    print(fit_params, 'fit parameters for ',i)
-    plt.scatter(allFrequencies[i], allPotentials[i], c='b', marker=markers[i], s=100)
-    plt.plot(freq_x, fit_Vs, label=str(percentages[1:6][i]))
-    plt.legend(loc='best')
-avg_slope = np.mean(slopes)
-avg_yint = np.mean(yints)
-print('Average slope of Vs vs Freq: '+str(avg_slope))
-print('Average y intercept of Vs vs Freq: '+str(avg_yint))
-    
-#plt.scatter(percentages[1:6],stoppingPotentials_threshold)
-
+plt.scatter(frequencies, stoppingPotentials_intercept, c='b', marker='*', s=100)
+plt.scatter(frequencies, stoppingPotentials_threshold, c='r')
+plt.scatter(frequencies, stoppingPotentials_diodefit, c='y')
+plt.scatter(frequencies, stoppingPotentials_xintFit, c='g')
 #plt.plot(voltageArrays,currentArrays)
 plt.show()
